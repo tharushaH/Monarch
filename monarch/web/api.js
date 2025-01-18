8885c43fa54716a28739b6b7cdc85675305b9a80
Tharusha Herath <tharusha.herath13@gmail.com>
2025-01-18 11:54:35 -0500

    Fixing repo errors.

--- monarch/web/api.js
+++
@@ -1,5 +0,0 @@
-// Sets up the API client for interacting with your backend. 
-// For your API reference, visit: https://docs.gadget.dev/api/monarch
-import { Client } from "@gadget-client/monarch";
-
-export const api = new Client({ environment: window.gadgetConfig.environment });
